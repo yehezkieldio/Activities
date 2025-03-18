8b0d91d123cbc525cf356b8f9bfbde8455ba5e52
Bas van Zanten <me@bas950.com>
2025-03-18 19:50:05 +0100

    feat(OnlyHit): recode activity (#9404)

--- websites/O/OnlyHit/presence.ts
+++ websites/O/OnlyHit/presence.ts
@@ -1,184 +1,76 @@
-import { Assets } from 'premid'
+import { ActivityType, Assets, getTimestamps } from 'premid'
 
 const presence = new Presence({
   clientId: '666412985513672715',
 })
-const browsingTimestamp = Math.floor(Date.now() / 1000)
-const strings = presence.getStrings({
-  play: 'general.playing',
-  pause: 'general.paused',
-})
-
-// ! Songs timestamp will reset on new song (see further below)
-let songTimestamp = Math.floor(Date.now() / 1000)
-let currentTitle = ''
-let lastTitle = ''
-
-enum ActivityAssets {
-  Gold = 'https://cdn.rcd.gg/PreMiD/websites/O/OnlyHit/assets/0.png',
-  Logo = 'https://cdn.rcd.gg/PreMiD/websites/O/OnlyHit/assets/1.png',
-  Japan = 'https://cdn.rcd.gg/PreMiD/websites/O/OnlyHit/assets/2.png',
-  Kpop = 'https://cdn.rcd.gg/PreMiD/websites/O/OnlyHit/assets/3.png',
-}
 
 presence.on('UpdateData', async () => {
   const presenceData: PresenceData = {}
-  const [format1, format2, showElapsed] = await Promise.all([
+  const [format1, format2] = await Promise.all([
     presence.getSetting<string>('sFormat1'),
     presence.getSetting<string>('sFormat2'),
-    presence.getSetting<boolean>('tElapsed'),
   ])
+  const strings = await presence.getStrings({
+    play: 'general.playing',
+    pause: 'general.paused',
+    live: 'general.live',
+    buttonListenAlong: 'general.buttonListenAlong',
+    buttonViewSong: 'general.buttonViewSong',
+  })
 
-  // ! Merch website
-  if (document.location.hostname === 'onlyhit.merchforall.com') {
-    //* Show timestamp if the setting is enabled and set largeImageKey
-    if (showElapsed)
-      presenceData.startTimestamp = browsingTimestamp
-
-    presenceData.largeImageKey = ActivityAssets.Logo
-    presenceData.smallImageKey = Assets.Reading
-
-    //* If they have site information enabled
-    if (document.location.hash.includes('/cart')) {
-      presenceData.details = 'Store - Viewing cart'
-    }
-    else if (document.location.pathname === '/') {
-      if (document.querySelector('.popup-container.active')) {
-        presenceData.details = 'Store - Viewing product:'
-        presenceData.state = document.querySelector(
-          '.popup-container.active .product-title',
-        )?.textContent
-      }
-      else {
-        presenceData.details = 'Browsing through'
-        presenceData.state = 'the store...'
-      }
-    }
-  }
-  else {
-    // ! Normal website
-    //* Set largeImageKey to the radio type
-    switch (document.querySelector('.stream-name')?.textContent) {
-      case 'OnlyHit Gold':
-        presenceData.largeImageKey = ActivityAssets.Gold
-        break
-      case 'OnlyHit Japan':
-        presenceData.largeImageKey = ActivityAssets.Japan
-        break
-      case 'OnlyHit K-Pop':
-        presenceData.largeImageKey = ActivityAssets.Kpop
-        break
-      default:
-        presenceData.largeImageKey = ActivityAssets.Logo
-        break
-    }
-
-    //* Get track information
-    const artist = document.querySelector('.artist')?.textContent
-    const title = document.querySelector('.title')?.textContent
-    const paused = document.querySelector<HTMLElement>('.fa-pause.pause-button')?.style.cssText === 'display: none;'
-
-    //* Set state details and image to track information.
-    presenceData.details = format1
-      .replace('%song%', title ?? '')
-      .replace('%artist%', artist ?? '')
-    presenceData.state = format2
-      .replace('%song%', title ?? '')
-      .replace('%artist%', artist ?? '')
-    presenceData.smallImageKey = paused ? Assets.Pause : Assets.Play
-    presenceData.smallImageText = paused
-      ? (await strings).pause
-      : (await strings).play
-
-    //* Refresh timestamp if a new song is playing
-    currentTitle = title ?? ''
-    if (currentTitle !== lastTitle) {
-      lastTitle = currentTitle
-      songTimestamp = Math.floor(Date.now() / 1000)
-    }
-
-    //* Show timestamp if the setting is enabled
-    if (showElapsed)
-      presenceData.startTimestamp = songTimestamp
-    else delete presenceData.startTimestamp
-
-    //* If they have site information enabled
-    // ! Check if user is on homepage or not
-    if (
-      document.location.pathname !== '/'
-      && !document.location.pathname.includes('video-version')
-    ) {
-      //* Show timestamp if the setting is enabled
-      if (showElapsed)
-        presenceData.startTimestamp = browsingTimestamp
-      else delete presenceData.startTimestamp
-
-      //* Get page title, and set smallImageText to track information
-      const page = document.querySelector('.main_title')?.textContent?.trim()
-      presenceData.smallImageText = `"${title}" by ${artist}`
-
-      //* Show page information
-      if (document.location.pathname.includes('/contact')) {
-        presenceData.details = 'Contacting OnlyHit'
-        delete presenceData.state
-        presenceData.smallImageKey = Assets.Writing
-      }
-      else if (page?.includes('Request a Song')) {
-        presenceData.details = 'Requesting a song'
-        presenceData.state = `for ${page.split(' - ')[0]}`
-        presenceData.smallImageKey = Assets.Writing
-      }
-      else if (document.location.pathname.includes('/programs/')) {
-        presenceData.details = 'Viewing program:'
-        presenceData.state = page
-        presenceData.smallImageKey = Assets.Reading
-      }
-      else if (document.location.pathname.includes('/programs')) {
-        presenceData.details = 'Browsing through'
-        presenceData.state = 'the upcoming programs'
-        presenceData.smallImageKey = Assets.Reading
-      }
-      else if (document.location.pathname.includes('/played-tracks')) {
-        presenceData.details = 'Browsing through the'
-        presenceData.state = 'recently played tracks'
-        presenceData.smallImageKey = Assets.Reading
-      }
-      else if (document.location.pathname.includes('/team/')) {
-        presenceData.details = 'Viewing OnlyHit team member:'
-        presenceData.state = page
-        presenceData.smallImageKey = Assets.Reading
-      }
-      else if (document.location.pathname.includes('/team')) {
-        presenceData.details = 'Viewing the OnlyHit Team'
-        delete presenceData.state
-        presenceData.smallImageKey = Assets.Reading
-      }
-      else if (document.location.pathname.includes('/where-to-listen')) {
-        presenceData.details = 'Viewing where you can'
-        presenceData.state = 'listen to OnlyHit'
-        presenceData.smallImageKey = Assets.Reading
-      }
-      else if (document.location.pathname.includes('/discord-bot')) {
-        presenceData.details = 'Viewing the Discord Bot'
-        delete presenceData.state
-        presenceData.smallImageKey = Assets.Reading
-      }
-      else if (document.location.pathname.includes('/search')) {
-        presenceData.details = 'Searching for:';
-        [, presenceData.state] = page?.split('"') ?? []
-        presenceData.smallImageKey = Assets.Search
-      }
-      else {
-        //* Show normal page information if there isn't a "special" one set above
-        presenceData.details = 'Viewing page:'
-        presenceData.state = page
-        presenceData.smallImageKey = Assets.Reading
+  const {
+    data,
+  } = await presence.getPageVariable<{
+    data: {
+      artist: string
+      title: string
+      isrc?: string
+      cover?: string
+      isPlaying: boolean
+      start_time: number
+      duration: number
+      station: {
+        name: string
+        logo: string
+        id: string
       }
     }
+  }>(
+    'data',
+  )
+
+  if (!data) {
+    presence.setActivity()
+    return
   }
 
-  //* Sets the presenceData, if there is no details it sets empty data (Which will still show "Playing OnlyHit")
-  if (presenceData.details)
-    presence.setActivity(presenceData)
-  else presence.setActivity()
+  presenceData.type = ActivityType.Listening
+  presenceData.name = data.station.name
+  presenceData.details = format1
+    .replace('%song%', data.title)
+    .replace('%artist%', data.artist)
+  presenceData.state = format2
+    .replace('%song%', data.title)
+    .replace('%artist%', data.artist)
+  presenceData.smallImageKey = data.isPlaying ? data.duration === 0 ? Assets.Live : Assets.Play : Assets.Pause
+  presenceData.smallImageText = data.isPlaying ? data.duration === 0 ? strings.live : strings.play : strings.pause
+  presenceData.largeImageKey = `https://onlyhit.us/cdn/${data.cover ? data.cover : data.station.logo}?width=512`
+  if (data.duration !== 0 && data.isPlaying) {
+    [presenceData.startTimestamp, presenceData.endTimestamp] = getTimestamps(Math.floor(Date.now() / 1000) - data.start_time, data.duration)
+  }
+
+  presenceData.buttons = [
+    {
+      label: strings.buttonListenAlong,
+      url: `https://onlyhit.us/en/radio/${data.station.id}`,
+    },
+  ]
+  if (data.isrc) {
+    presenceData.buttons.push({
+      label: strings.buttonViewSong,
+      url: `https://onlyhit.us/en/music/track/${data.isrc}`,
+    })
+  }
+
+  presence.setActivity(presenceData)
 })
