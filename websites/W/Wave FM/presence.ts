8bca84f18908961458dbe2b3ff212b60f89ae772
joão f.s. neto <finnzzingames758@gmail.com>
2025-03-28 17:05:57 -0300

    feat(Wave FM): add activity (#9427)
    Signed-off-by: joão f.s. neto <finnzzingames758@gmail.com>
    Co-authored-by: Daniel Lau <32113157+theusaf@users.noreply.github.com>

---
+++ websites/W/Wave FM/presence.ts
@@ -0,0 +1,61 @@
+import { ActivityType, Assets } from 'premid'
+
+const presence = new Presence({
+  clientId: '1343395168530726962',
+})
+const strings = presence.getStrings({
+  play: 'general.playing',
+  pause: 'general.paused',
+})
+const browsingTimestamp = Math.floor(Date.now() / 1000)
+
+enum ActivityAssets {
+  Logo = 'https://i.imgur.com/cSmc3k0.png',
+  LogoPop = 'https://i.imgur.com/WysR2og.png',
+}
+
+presence.on('UpdateData', async () => {
+  const player = document.querySelector('#pause-btn')?.getAttribute('style') === 'display: inline;'
+  const artistName = document.querySelector('#song-artist')?.textContent
+  const musicName = document.querySelector('#song-title')?.textContent
+  const presenceData: PresenceData = {
+    largeImageKey: document.querySelector<HTMLImageElement>('#song-image')?.src,
+    startTimestamp: browsingTimestamp,
+  }
+
+  if (document.location.pathname === '/') {
+    if (player === true) {
+      presenceData.type = ActivityType.Listening
+      presenceData.details = musicName
+      presenceData.state = artistName
+      presenceData.smallImageKey = Assets.Play
+      presenceData.smallImageText = (await strings).play
+    }
+    else {
+      presenceData.largeImageKey = ActivityAssets.Logo
+      presenceData.details = 'Página inicial'
+      presenceData.smallImageKey = Assets.Pause
+      presenceData.smallImageText = (await strings).pause
+    }
+  }
+  else if (document.location.pathname === '/pop/') {
+    if (player === true) {
+      presenceData.type = ActivityType.Listening
+      presenceData.name = 'WavePop'
+      presenceData.details = musicName
+      presenceData.state = artistName
+      presenceData.smallImageKey = Assets.Play
+      presenceData.smallImageText = (await strings).play
+    }
+    else {
+      presenceData.name = 'WavePop'
+      presenceData.largeImageKey = ActivityAssets.LogoPop
+      presenceData.smallImageKey = Assets.Pause
+      presenceData.details = 'Página inicial'
+      presenceData.smallImageText = (await strings).pause
+    }
+  }
+  if (!presenceData.details)
+    presence.setActivity()
+  else presence.setActivity(presenceData)
+})
