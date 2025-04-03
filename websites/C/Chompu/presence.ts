28ad97dada7e850e59870fcc8dcd4c0c344cc073
SomboyTiger <103308315+somboytiger@users.noreply.github.com>
2025-04-03 15:33:39 +0700

    feat(Chompu): update activity (#9442)
    * update rebuild page
    
    * remove tags dashboard, music-room
    
    * add smallImagekey reading

--- websites/C/Chompu/presence.ts
+++ websites/C/Chompu/presence.ts
@@ -1,4 +1,4 @@
-import { ActivityType, Assets } from 'premid'
+import { ActivityType, Assets, getTimestamps, timestampFromFormat } from 'premid'
 
 const presence = new Presence({
   clientId: '1219713910165209169',
@@ -9,126 +9,49 @@ enum ActivityAssets {
   Logo = 'https://cdn.rcd.gg/PreMiD/websites/C/Chompu/assets/logo.png',
 }
 
-enum Pages {
-  Home = '/',
-  Dashboard = '/dashboard',
-  Status = '/status',
-  Contact = '/contact',
-}
-
 const presenceData: PresenceData = {
   type: ActivityType.Listening,
   largeImageKey: ActivityAssets.Logo,
 }
 
 presence.on('UpdateData', async () => {
-  const base = document.location.pathname
+  if (document.querySelector<HTMLElement>('[data-player-status=\'true\']')) {
+    const playing = document.querySelector<HTMLButtonElement>('button svg[data-player-pause="true"]')
+    const [startPlayer, durationPlayer] = [
+      timestampFromFormat(
+        document.querySelector<HTMLElement>('[data-player-position="true"]')?.textContent?.split(' / ')[0] ?? '',
+      ),
+      timestampFromFormat(document.querySelector<HTMLElement>('[data-player-position="true"]')?.textContent?.split(' / ')[1] ?? ''),
+    ]
+    const [startTimestamp, endTimestamp] = getTimestamps(
+      startPlayer,
+      durationPlayer,
+    );
 
-  if (/\/dashboard\/guild\/[^\d\n\r_\u2028\u2029]*[\d_].*\/music-room/i.test(base)) {
-    if (
-      document.querySelector<HTMLElement>('div.hidden.-player-status')
-      && document.querySelector<HTMLElement>('div.hidden.-player-status')
-        ?.textContent === 'true'
-    ) {
-      const author = document.querySelector<HTMLAnchorElement>(
-        'p.text-small.mt-1.text-foreground\\/80.-player-author',
-      )
-      const playing = document.querySelector<HTMLAnchorElement>(
-        'svg.-player-playing',
-      )
-      const timeEndPlayer = document.querySelector<HTMLElement>(
-        'p.text-small.text-foreground\\/50.-player-position-end',
-      )?.textContent
-      const [startPlayer, durationPlayer] = [
-        presence.timestampFromFormat(
-          document.querySelector<HTMLElement>(
-            'p.text-small.-player-position-start',
-          )?.textContent ?? '',
-        ),
-        presence.timestampFromFormat(timeEndPlayer ?? ''),
-      ]
-      const [startTimestamp, endTimestamp] = presence.getTimestamps(
-        startPlayer,
-        durationPlayer,
-      );
+    [presenceData.startTimestamp, presenceData.endTimestamp] = [
+      startTimestamp,
+      endTimestamp,
+    ]
 
-      [presenceData.startTimestamp, presenceData.endTimestamp] = [
-        startTimestamp,
-        endTimestamp,
-      ]
-
-      presenceData.details = document.querySelector<HTMLAnchorElement>(
-        'h1.text-large.font-medium.-player-title',
-      )
-      presenceData.state = author
-      presenceData.largeImageKey = document.querySelector<HTMLImageElement>(
-        '[data-label=\'guild-logo\']',
-      )?.src
-      presenceData.smallImageKey = playing ? Assets.Play : Assets.Pause
-      presenceData.smallImageText = playing ? 'Playing' : 'Pause'
-      presenceData.startTimestamp = startTimestamp
-      presenceData.endTimestamp = endTimestamp
-      presenceData.buttons = [
-        {
-          label: `Join Player ${
-            document.querySelector<HTMLAnchorElement>(
-              '[data-label=\'player-requester\']',
-            )?.textContent
-          }`,
-          url: document.location.href,
-        },
-      ]
-
-      if (!playing)
-        delete presenceData.startTimestamp
-    }
-    else {
-      presenceData.details = 'No song queue found'
-      presenceData.state = 'In the server...'
-      presenceData.largeImageKey = document.querySelector<HTMLImageElement>(
-        '[data-label=\'guild-logo\']',
-      )
-        ? document.querySelector<HTMLImageElement>('[data-label=\'guild-logo\']')
-          ?.src
-        : ActivityAssets.Logo
-      presenceData.smallImageText = 'Zzz'
-      presenceData.startTimestamp = browsingTimestamp
-      presenceData.buttons = [
-        {
-          label: 'Join Player',
-          url: document.location.href,
-        },
-      ]
-      if (presenceData.endTimestamp)
-        delete presenceData.endTimestamp
-    }
+    presenceData.details = document.querySelector<HTMLElement>('[data-player-title]')?.textContent
+    presenceData.state = document.querySelector<HTMLElement>('[data-player-author]')?.textContent
+    presenceData.largeImageKey = document.querySelector<HTMLImageElement>('[data-player-image]')?.src
+    presenceData.smallImageKey = playing ? Assets.Play : Assets.Pause
+    presenceData.smallImageText = playing ? 'Playing' : 'Pause'
+    presenceData.startTimestamp = startTimestamp
+    presenceData.endTimestamp = endTimestamp
+    if (!playing)
+      delete presenceData.startTimestamp
   }
   else {
-    presenceData.details = 'Idk'
-    presenceData.state = 'Browsing...'
+    presenceData.details = 'No song queue found'
+    presenceData.state = 'In the server...'
     presenceData.largeImageKey = ActivityAssets.Logo
     presenceData.smallImageKey = Assets.Reading
     presenceData.smallImageText = 'Zzz'
     presenceData.startTimestamp = browsingTimestamp
-    if (presenceData.buttons)
-      delete presenceData.buttons
     if (presenceData.endTimestamp)
       delete presenceData.endTimestamp
-
-    switch (base) {
-      case Pages.Home:
-        presenceData.details = 'Home'
-        break
-      case Pages.Dashboard:
-        presenceData.details = 'Dashboard'
-        break
-      case Pages.Status:
-        presenceData.details = 'Status'
-        break
-      case Pages.Contact:
-        presenceData.details = 'Contact'
-        break
-    }
   }
 
   presence.setActivity(presenceData)
