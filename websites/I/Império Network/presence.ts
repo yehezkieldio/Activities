b707da3ded889bae5b5dafa87c3baddeded45f92
Bruno Ramos <37979213+Kuriel23@users.noreply.github.com>
2025-04-13 14:02:16 +0100

    feat(Império Network): add activity (#9465)

---
+++ websites/I/Império Network/presence.ts
@@ -0,0 +1,72 @@
+const presence = new Presence({
+  clientId: '882761234271834142',
+})
+const browsingTimestamp = Math.floor(Date.now() / 1000)
+
+enum ActivityAssets {
+  Logo = 'https://i.imgur.com/ha1c7MT.png',
+}
+
+async function getStrings() {
+  return presence.getStrings({
+    search: 'general.searchFor',
+    viewHome: 'general.viewHome',
+    buttonViewPage: 'general.buttonViewPage',
+    read: 'general.readingArticle',
+    viewCategory: 'general.viewCategory',
+    browsing: 'general.browsing',
+    readingAbout: 'general.readingAbout',
+  })
+}
+
+presence.on('UpdateData', async () => {
+  const presenceData: PresenceData = {
+    largeImageKey: ActivityAssets.Logo,
+    startTimestamp: browsingTimestamp,
+  }
+  const strings = await getStrings()
+  const { pathname, href, search } = document.location
+
+  switch (pathname.split('/')[1]) {
+    case 'categoria': {
+      presenceData.details = strings.viewCategory
+      presenceData.state = document.querySelector('#posts-grid > li > a > article > div > div > a')
+      break
+    }
+    case 'post': {
+      presenceData.details = strings.read
+      presenceData.state = document.querySelector('h1.flex')
+      presenceData.largeImageKey = document.querySelector<HTMLImageElement>('.object-cover') ?? ActivityAssets.Logo
+      presenceData.buttons = [
+        {
+          label: strings.buttonViewPage,
+          url: href,
+        },
+      ]
+      break
+    }
+    case 'review-policy':
+    case 'privacy':
+    case 'sobre':
+    case 'contato': {
+      presenceData.details = strings.readingAbout
+      presenceData.state = document.querySelector('h1.text-2xl')
+      break
+    }
+    case 'search': {
+      presenceData.details = strings.search
+      presenceData.state = decodeURIComponent(
+        search?.split('=')[1] ?? '',
+      )
+      break
+    }
+    default: {
+      presenceData.details = strings.viewHome
+      if (pathname.split('/')[1])
+        presenceData.details = strings.browsing
+      break
+    }
+  }
+
+  presence.setActivity(presenceData)
+})
