127338c4102fefe9e6278efe09498028849d8a10
PreMiD Automations <contact@premid.app>
2025-04-28 17:23:17 +0000

    chore: update assets

--- websites/T/TruckersFM/presence.ts
+++ websites/T/TruckersFM/presence.ts
@@ -7,7 +7,7 @@ const browsingTimestamp = Math.floor(Date.now() / 1000)
 
 presence.on('UpdateData', () => {
   const presenceData: PresenceData = {
-    largeImageKey: document.querySelector<HTMLImageElement>('.album-art')?.src || 'https://i.imgur.com/ZqheZoX.png',
+    largeImageKey: document.querySelector<HTMLImageElement>('.album-art')?.src || 'https://cdn.rcd.gg/PreMiD/websites/T/TruckersFM/assets/logo.png',
     startTimestamp: browsingTimestamp,
     type: ActivityType.Listening,
   }
@@ -16,7 +16,7 @@ presence.on('UpdateData', () => {
     document.querySelector('.player-artist-text')?.textContent
   } - ${document.querySelector('.player-title-text')?.textContent}`
   presenceData.state = document.querySelector('.live-name')?.textContent ?? 'AutoDJ'
-  presenceData.smallImageKey = 'https://i.imgur.com/ZqheZoX.png'
+  presenceData.smallImageKey = 'https://cdn.rcd.gg/PreMiD/websites/T/TruckersFM/assets/logo.png'
   presenceData.smallImageText = 'TruckersFM'
 
   presence.setActivity(presenceData)
