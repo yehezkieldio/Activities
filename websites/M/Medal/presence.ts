c4e941ffe711ebc5385cccea80266046ec832172
PreMiD Automations <contact@premid.app>
2025-04-29 21:51:42 +0000

    chore: update assets

--- websites/M/Medal/presence.ts
+++ websites/M/Medal/presence.ts
@@ -5,7 +5,7 @@ const presence = new Presence({
 })
 
 enum ActivityAssets {
-  Logo = 'https://i.imgur.com/tF6xSsN.png',
+  Logo = 'https://cdn.rcd.gg/PreMiD/websites/M/Medal/assets/logo.png',
 }
 
 presence.on('UpdateData', async () => {
