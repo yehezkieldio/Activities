00b1878eabd9d012ec6d3c21dcba64aff9b5c7c1
Muhammad Rizki Syahputra <madriztra@gmail.com>
2025-05-14 17:11:28 +0700

    feat(SkuyFun): add activity (#9540)

---
+++ websites/S/skuyfun/presence.ts
@@ -0,0 +1,33 @@
+import { ActivityType } from 'premid'
+
+const presence = new Presence({
+  clientId: '1369034572859445399',
+})
+
+const LOGO_IMAGE_URL = 'https://i.ibb.co.com/cXMm9zNY/logo.png' // largeImage Logo if title not detected
+
+presence.on('UpdateData', async () => {
+  const title = document.querySelector('title')?.textContent?.trim()
+  const episode = document.querySelector('h4 span:nth-child(2)')?.textContent
+  const imgElement = document.querySelector<HTMLImageElement>('img.rounded-md')
+
+  let presenceData
+
+  if (title && episode && imgElement) {
+    presenceData = {
+      type: ActivityType.Watching,
+      details: `Watching: ${title}`,
+      state: `Episode: ${episode}`,
+      largeImageKey: imgElement.src,
+    }
+  }
+  else {
+    presenceData = {
+      type: ActivityType.Watching,
+      details: 'Browsing...',
+      largeImageKey: LOGO_IMAGE_URL,
+    }
+  }
+
+  presence.setActivity(presenceData)
+})
