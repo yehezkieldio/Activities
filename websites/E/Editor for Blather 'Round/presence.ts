8f51607c44d764679497de6e40e3e2a67cc6df26
PreMiD Automations <contact@premid.app>
2025-05-20 23:32:25 +0000

    chore: update assets

--- websites/E/Editor for Blather 'Round/presence.ts
+++ websites/E/Editor for Blather 'Round/presence.ts
@@ -6,7 +6,7 @@ const presence = new Presence({
 const browsingTimestamp = Math.floor(Date.now() / 1000)
 
 enum ActivityAssets {
-  Logo = 'https://i.imgur.com/rFZupTH.png',
+  Logo = 'https://cdn.rcd.gg/PreMiD/websites/E/Editor%20for%20Blather%20%27Round/assets/logo.png',
 }
 
 presence.on('UpdateData', async () => {
