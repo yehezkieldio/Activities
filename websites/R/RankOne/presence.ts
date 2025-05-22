2ff516143d76e291df0b758249a05b89fac39d2a
PreMiD Automations <contact@premid.app>
2025-05-22 09:41:15 +0000

    chore: update assets

--- websites/R/RankOne/presence.ts
+++ websites/R/RankOne/presence.ts
@@ -4,7 +4,7 @@ const presence = new Presence({
 const browsingTimestamp = Math.floor(Date.now() / 1000)
 
 enum ActivityAssets {
-  Logo = 'https://i.imgur.com/dRN96t2.png',
+  Logo = 'https://cdn.rcd.gg/PreMiD/websites/R/RankOne/assets/logo.png',
 }
 
 async function getStrings() {
