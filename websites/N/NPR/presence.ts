d57344a4f27c0cd30171c0dce94700d5900d6999
PreMiD Automations <contact@premid.app>
2025-05-22 20:58:02 +0000

    chore: update assets

--- websites/N/NPR/presence.ts
+++ websites/N/NPR/presence.ts
@@ -11,7 +11,7 @@ const presence = new Presence({
 const browsingTimestamp = Math.floor(Date.now() / 1000)
 
 enum ActivityAssets {
-  Logo = 'https://i.imgur.com/BdabueB.png',
+  Logo = 'https://cdn.rcd.gg/PreMiD/websites/N/NPR/assets/logo.png',
 }
 
 presence.on('UpdateData', async () => {
