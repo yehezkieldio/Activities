9876fab2a7edc9e5eae6e6f8814b37184534ed11
Linext <nutlicker28@gmail.com>
2025-06-04 09:37:00 -0400

    feat:(AnimeKai): update supported urls (#9674)
    Add AnimeKai.bz to supported URLs list
    Add functionality while veiwing own boorkmarks
    Add functionality while looking at notifications

--- websites/A/AnimeKai/presence.ts
+++ websites/A/AnimeKai/presence.ts
@@ -31,7 +31,7 @@ presence.on(
 
 presence.on('UpdateData', async () => {
   const presenceData: PresenceData = {
-    name: 'AnimeKai',
+    name: 'AnimeKAI',
     largeImageKey: ActivityAssets.Logo,
     startTimestamp: browsingTimestamp,
   }
@@ -39,7 +39,7 @@ presence.on('UpdateData', async () => {
   const buttons = await presence.getSetting<boolean>('buttons')
 
   if (pathname === '/' || pathname === '/home') {
-    presenceData.details = 'Exploring AnimeKai.to'
+    presenceData.details = 'Exploring AnimeKAI'
   }
   else if (
     /\/(?:ongoing|recent|movie|new-releases|updates|tv|completed|top-upcoming|ona|ova|special|genres\/.*)/.test(
@@ -184,6 +184,16 @@ presence.on('UpdateData', async () => {
       case '/user/import': {
         presenceData.details = 'MAL Import/Export'
         presenceData.smallImageKey = Assets.Downloading
+        break
+      }
+      case '/user/notifications': {
+        presenceData.details = 'Looking at Notifications'
+        presenceData.smallImageKey = Assets.Reading
+        break
+      }
+      case '/user/bookmarks': {
+        presenceData.details = 'Managing Bookmarks'
+        presenceData.smallImageKey = ActivityAssets.Settings
         break
       }
       default: {
