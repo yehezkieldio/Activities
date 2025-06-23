919f6726eef8c068af62f65d75e1ddec94a4a914
Debangshu <146982673+debangshu919@users.noreply.github.com>
2025-06-23 14:51:59 +0530

    feat(Enka.Network): add activity (#9725)
    Signed-off-by: Debangshu <146982673+debangshu919@users.noreply.github.com>
    Co-authored-by: Daniel Lau <32113157+theusaf@users.noreply.github.com>

---
+++ websites/E/Enka.Network/presence.ts
@@ -0,0 +1,108 @@
+const presence = new Presence({
+  clientId: '1385584175167312005',
+})
+const browsingTimestamp = Math.floor(Date.now() / 1000)
+
+enum ActivityAssets {
+  Logo = 'https://res.cloudinary.com/dg9yrubdu/image/upload/w_512,h_512,c_fill/enka-network-logo.png',
+  genshinIcon = 'https://res.cloudinary.com/dg9yrubdu/image/upload/w_512,h_512,c_fill/gi-icon.png',
+  hsrIcon = 'https://res.cloudinary.com/dg9yrubdu/image/upload/w_512,h_512,c_fill/hsr-icon.png',
+  zzzIcon = 'https://res.cloudinary.com/dg9yrubdu/image/upload/w_512,h_512,c_fill/zzz-icon.png',
+}
+
+function fetchPlayerDetails(): { name: string, level: string } {
+  return {
+    name: document.querySelector('div.PlayerInfo div.details h1')!.textContent!,
+    level: document.querySelector('div.PlayerInfo div.details div.ar')!.textContent!.split(' ').slice(0, 2).join(' '),
+  }
+}
+
+presence.on('UpdateData', async () => {
+  const page = document.location.pathname
+  const [, ...hrefRest] = document.location.href.split('//')
+  const href = hrefRest.join('')
+
+  const presenceData: PresenceData = {
+    largeImageKey: ActivityAssets.Logo,
+    startTimestamp: browsingTimestamp,
+  }
+
+  // Genshin Impact
+  if (
+    href.includes('enka.network/u/')
+    && href.match(/enka\.network\/u\/(\d+)/)
+  ) {
+    const UID = href.match(/enka\.network\/u\/(\d+)/)![1]
+    const characterName = document.querySelector('div.name')?.childNodes[0]?.nodeValue?.trim()
+    const player = fetchPlayerDetails()
+
+    presenceData.details = `Viewing ${characterName} build`
+    presenceData.state = `${player.name} | ${player.level}`
+    presenceData.smallImageText = `UID: ${UID}` || `Genshin Impact`
+    presenceData.smallImageKey = ActivityAssets.genshinIcon
+  }
+
+  // Honkai: Star Rail
+  else if (
+    href.includes('enka.network/hsr/')
+    && href.match(/enka\.network\/hsr\/(\d+)/)
+  ) {
+    const UID = href.match(/enka\.network\/hsr\/(\d+)/)![1]
+    const characterName = document.querySelector('div.name')?.textContent
+    const player = fetchPlayerDetails()
+
+    presenceData.details = `Viewing ${characterName} build`
+    presenceData.state = `${player.name} | ${player.level}`
+    presenceData.smallImageText = `UID: ${UID}` || `Honkai: Star Rail`
+    presenceData.smallImageKey = ActivityAssets.hsrIcon
+  }
+
+  // Zenless Zone Zero
+  else if (
+    href.includes('enka.network/zzz/')
+    && href.match(/enka\.network\/zzz\/(\d+)/)
+  ) {
+    const UID = href.match(/enka\.network\/zzz\/(\d+)/)![1]
+    const characterName = document.querySelector('div.name')?.textContent
+    const player = fetchPlayerDetails()
+
+    presenceData.details = `Viewing ${characterName} build`
+    presenceData.state = `${player.name} | ${player.level}`
+    presenceData.smallImageText = `UID: ${UID}` || `Zenless Zone Zero`
+    presenceData.smallImageKey = ActivityAssets.zzzIcon
+  }
+
+  // Homepage
+  else if (
+    page === '/'
+  ) {
+    presenceData.details = 'Viewing homepage'
+    if (document.querySelector('div.hero h1')?.textContent?.includes('Genshin Impact')) {
+      presenceData.state = 'Genshin Impact'
+    }
+    else if (document.querySelector('div.hero h1')?.textContent?.includes('Honkai: Star Rail')) {
+      presenceData.state = 'Honkai: Star Rail'
+    }
+    else if (document.querySelector('div.hero h1')?.textContent?.includes('ZZZ')) {
+      presenceData.state = 'Zenless Zone Zero'
+    }
+  }
+
+  // Profile
+  else if (
+    href.match(/enka\.network\/u\/[^/]+/)
+  ) {
+    const username = href.match(/enka\.network\/u\/([^/]+)/)![1]
+    presenceData.details = 'Viewing a profile'
+    presenceData.state = username
+  }
+
+  // Settings
+  else if (
+    href.includes('enka.network/profile/settings/')
+  ) {
+    presenceData.details = 'Viewing profile settings'
+  }
+
+  presence.setActivity(presenceData)
+})
