9b35a9cae1a75c46b76562b6c0d219a72af4a17d
Ayphyer <mail.an.addy@gmx.de>
2025-07-10 20:20:42 +0200

    feat(AlternativeScans): add activity (#9732)
    * Add files via upload
    
    Signed-off-by: Ayphyer <mail.an.addy@gmx.de>
    
    * Add files via upload
    
    Signed-off-by: Ayphyer <mail.an.addy@gmx.de>
    
    * Update presence.ts
    
    Signed-off-by: Ayphyer <mail.an.addy@gmx.de>
    
    * Update presence.ts
    
    Change parameter
    
    Signed-off-by: Ayphyer <mail.an.addy@gmx.de>
    
    ---------
    
    Signed-off-by: Ayphyer <mail.an.addy@gmx.de>

---
+++ websites/A/AlternativeScans/presence.ts
@@ -0,0 +1,98 @@
+import { ActivityType, Assets } from 'premid'
+
+const presence = new Presence({
+  clientId: '1382462713682460672',
+})
+const browsingTimestamp = Math.floor(Date.now() / 1000)
+
+enum ActivityAssets {
+  Logo = 'https://i.imgur.com/O9MN9Th.png',
+}
+
+presence.on('UpdateData', async () => {
+  const presenceData: PresenceData = {
+    largeImageKey: ActivityAssets.Logo,
+    startTimestamp: browsingTimestamp,
+    smallImageKey: Assets.Search,
+    type: ActivityType.Watching,
+  }
+  const { pathname, href } = document.location
+  const pathArr = pathname.split('/')
+  let pathred
+  let img
+  let titlename
+
+  if (pathArr[1]) {
+    pathred = pathArr[1].split('?')
+
+    switch (pathArr[1]) {
+      case 'series':
+        img = document.querySelector<HTMLImageElement>('.cover-column img.cover-image')
+
+        presenceData.largeImageKey = img?.src ?? ''
+        presenceData.smallImageKey = Assets.Viewing
+
+        titlename = document
+          .querySelector('head > title')
+          ?.textContent
+
+        presenceData.details = `Viewing:  ${titlename}`
+        presenceData.buttons = [{
+          label: 'View Series',
+          url: href,
+        }]
+        break
+
+      case 'routes':
+        presenceData.details = `Searching for Series`
+
+        break
+
+      default:
+
+        if (pathred[0] === 'reader') {
+          const currentUrl = new URL(href)
+          const params = currentUrl.searchParams
+          const seriesparam = params.get('series')
+          const chapterparam = params.get('chapter')
+          const idparam = params.get('id')
+          let seriesparamup
+
+          if (seriesparam) {
+            seriesparamup = seriesparam.toUpperCase()
+          }
+
+          const name = document
+            .querySelector<HTMLAnchorElement>('#current-chapter-name a')
+            ?.textContent
+            ?.trim()
+            ?? ''
+
+          presenceData.details = `${name}`
+          presenceData.state = document
+            .querySelector('head > title')
+            ?.textContent
+            ?.replace(` - ${seriesparamup}`, '')
+          presenceData.largeImageKey = `https://files.alternativescans.icu/file/public-chapter-images/${seriesparam}/${chapterparam}/1.png`
+          presenceData.smallImageKey = Assets.Reading
+          presenceData.buttons = [{
+            label: 'Read Chapter',
+            url: href,
+          }, {
+            label: 'View Series',
+            url: `https://alternativescans.icu/series/?series=${seriesparam}&id=${idparam}`,
+          }]
+        }
+    }
+  }
+  else {
+    presenceData.details = 'Viewing the Homepage'
+  }
+
+  if (presenceData.details) {
+    presence.setActivity(presenceData)
+  }
+  else {
+    presence.clearActivity()
+  }
+})
