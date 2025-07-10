ac8cfd84f4ea9fb7f48f155b4d2e5ae1ba2cfd08
yichen_0403 <104032good@gmail.com>
2025-07-11 03:23:14 +0900

    feat(巴哈姆特動畫瘋): update activity image and details (#9742)
    * feat(巴哈姆特動畫瘋): update activity image and details and fix media timestamps
    
    * feat(巴哈姆特動畫瘋): update activity image and details, fix media timestamps, localization
    
    * Update presence.ts
    
    * Update presence.ts
    
    * Update presence.ts
    
    * Update presence.ts
    
    * Update presence.ts
    
    * feat(巴哈姆特動畫瘋): add anime party support
    
    * feat(巴哈姆特動畫瘋): update anime party state
    
    * Update presence.ts
    
    * Update presence.ts
    
    * Update presence.ts
    
    * remove unused code
    
    * fix: update version and correct the text

--- websites/#/巴哈姆特動畫瘋/presence.ts
+++ websites/#/巴哈姆特動畫瘋/presence.ts
@@ -1,4 +1,4 @@
-import { ActivityType, Assets } from 'premid'
+import { ActivityType, Assets, getTimestampsFromMedia } from 'premid'
 
 const presence = new Presence({
   clientId: '640194732718292992',
@@ -6,6 +6,8 @@ const presence = new Presence({
 const strings = presence.getStrings({
   play: 'general.playing',
   pause: 'general.paused',
+  browsing: 'general.browsing',
+  browseanime: 'general.buttonViewAnime',
 })
 const browsingTimestamp = Math.floor(Date.now() / 1000)
 
@@ -15,20 +17,19 @@ enum ActivityAssets {
 
 presence.on('UpdateData', async () => {
   const presenceData: PresenceData = {
-    type: ActivityType.Playing,
-    largeImageKey: ActivityAssets.Logo,
+    type: ActivityType.Watching,
   } as PresenceData
 
   if (document.location.hostname === 'ani.gamer.com.tw') {
     if (document.location.pathname === '/') {
       presenceData.startTimestamp = browsingTimestamp
-      presenceData.details = 'Viewing home page'
+      presenceData.details = '首頁'
     }
     else if (document.querySelector('#ani_video_html5_api')) {
       const video = document.querySelector<HTMLVideoElement>(
         '#ani_video_html5_api',
       )!;
-      [presenceData.startTimestamp, presenceData.endTimestamp] = presence.getTimestampsfromMedia(video)
+      [presenceData.startTimestamp, presenceData.endTimestamp] = getTimestampsFromMedia(video)
       if (!Number.isNaN(video.duration)) {
         presenceData.type = ActivityType.Watching
         presenceData.smallImageKey = video.paused ? Assets.Pause : Assets.Play
@@ -40,13 +41,29 @@ presence.on('UpdateData', async () => {
           '#BH_background > div.container-player > div.anime-title > div.anime-option > section.videoname > div.anime_name > h1',
         )
         presenceData.details = title?.textContent
+        presenceData.name = title?.textContent || '巴哈姆特動畫瘋'
 
-        const user = document.querySelector<HTMLElement>(
-          '#BH_background > div.container-player > div.anime-title > div.anime-option > section.videoname > div.anime_name > div > p',
+        const score = document.querySelector<HTMLElement>(
+          '.score-overall-number',
         )
 
-        if (user)
-          presenceData.state = user.textContent
+        const views = document.querySelector<HTMLElement>(
+          '.newanime-count span',
+        )
+
+        const image = document.querySelector<HTMLImageElement>('.data-file .data-img')?.getAttribute('data-src')
+        presenceData.largeImageKey = image || ActivityAssets.Logo
+        presenceData.largeImageText = 'PreMiD - 巴哈姆特動畫瘋'
+
+        if (score && views)
+          presenceData.state = `動畫瘋 | ✩${score.textContent} | ${views?.textContent}觀看`
+
+        presenceData.buttons = [
+          {
+            label: (await strings).browseanime,
+            url: document.location.href,
+          },
+        ]
 
         if (video.paused) {
           delete presenceData.startTimestamp
@@ -55,7 +72,7 @@ presence.on('UpdateData', async () => {
       }
       else if (Number.isNaN(video.duration)) {
         presenceData.startTimestamp = browsingTimestamp
-        presenceData.details = 'Looking at: '
+        presenceData.details = (await strings).browsing
         const title = document.querySelector(
           '#BH_background > div.container-player > div.anime-title > div.anime-option > section.videoname > div.anime_name > h1',
         )
@@ -63,15 +80,45 @@ presence.on('UpdateData', async () => {
         presenceData.smallImageKey = Assets.Reading
       }
     }
-    else if (document.location.pathname.includes('/animeList')) {
-      presenceData.startTimestamp = browsingTimestamp
-      presenceData.details = 'Viewing all animes'
+    else if (document.querySelector('#partyPlayer_html5_api')) {
+      const video = document.querySelector<HTMLVideoElement>(
+        '#partyPlayer_html5_api',
+      )!;
+      [presenceData.startTimestamp, presenceData.endTimestamp] = getTimestampsFromMedia(video)
+      if (!Number.isNaN(video.duration)) {
+        presenceData.type = ActivityType.Watching
+        presenceData.smallImageKey = video.paused ? Assets.Pause : Assets.Play
+        presenceData.smallImageText = video.paused
+          ? (await strings).pause
+          : (await strings).play
+
+        const data = document.querySelector<HTMLImageElement>(
+          '.ani-play-queue-item.is-playing .img-block img',
+        )
+
+        presenceData.largeImageKey = data?.getAttribute('data-src') || ActivityAssets.Logo
+        presenceData.largeImageText = 'PreMiD - 巴哈姆特動畫瘋'
+        presenceData.details = data?.getAttribute('alt')
+        presenceData.name = data?.getAttribute('alt') || '巴哈姆特動畫瘋'
+
+        const onlineViewers = document.querySelector<HTMLElement>(
+          '.people-count',
+        )
+        const queueList = document.querySelector('.ani-queue-list')
+        const upcomingEpisodes = queueList ? queueList.querySelectorAll('.ani-play-queue-item').length : 0
+        presenceData.state = `動畫派對 | ${onlineViewers?.textContent || ''}| ${upcomingEpisodes}集待看`
+
+        if (video.paused) {
+          delete presenceData.startTimestamp
+          delete presenceData.endTimestamp
+        }
+      }
     }
   }
 
   if (!presenceData.details) {
     presenceData.startTimestamp = browsingTimestamp
-    presenceData.details = 'Viewing page:'
+    presenceData.details = (await strings).browsing
     presenceData.state = document
       .querySelector('head > title')
       ?.textContent
