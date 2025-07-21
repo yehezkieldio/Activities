8d35c424ba3f763d8c15dbce19fdbd14e81386d1
PreMiD Automations <contact@premid.app>
2025-07-21 09:06:52 +0000

    chore: update assets

--- websites/M/ManhwaWeb/presence.ts
+++ websites/M/ManhwaWeb/presence.ts
@@ -7,7 +7,7 @@ const presence = new Presence({
 const browsingTimestamp = Math.floor(Date.now() / 1000)
 
 enum ActivityAssets {
-  Logo = 'https://i.imgur.com/kulV4oU.png',
+  Logo = 'https://cdn.rcd.gg/PreMiD/websites/M/ManhwaWeb/assets/logo.png',
 }
 
 async function getStrings() {
