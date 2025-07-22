e221e67a90a04611af2c517e7435b18e6c435b15
PreMiD Automations <contact@premid.app>
2025-07-22 11:54:07 +0000

    chore: update assets

--- websites/T/Taobao/presence.ts
+++ websites/T/Taobao/presence.ts
@@ -6,7 +6,7 @@ const presence = new Presence({
 const browsingTimestamp = Math.floor(Date.now() / 1000)
 
 enum ActivityAssets {
-  Logo = 'https://i.imgur.com/hhHDHt8.png',
+  Logo = 'https://cdn.rcd.gg/PreMiD/websites/T/Taobao/assets/logo.png',
 }
 
 presence.on('UpdateData', async () => {
