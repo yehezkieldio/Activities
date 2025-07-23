b5613a08747b3005354f2b998c31fb5429531c72
「CHN」かぐらざかなな <102936062+Kagurazaka-Nana@users.noreply.github.com>
2025-07-24 03:23:04 +0800

    feat(WatchPeopleLive): add activity (#9801)

---
+++ websites/W/WatchPeopleLive/presence.ts
@@ -0,0 +1,53 @@
+import { ActivityType } from 'premid'
+
+const presence = new Presence({
+  clientId: '1396529263095582890',
+})
+const browsingTimestamp = Math.floor(Date.now() / 1000)
+
+enum ActivityAssets {
+  Logo = 'https://i.imgur.com/tlpAcM8.png',
+}
+
+presence.on('UpdateData', async () => {
+  const presenceData: PresenceData = {
+    largeImageKey: ActivityAssets.Logo,
+    startTimestamp: browsingTimestamp,
+    type: ActivityType.Watching,
+    details: 'Viewing other pages',
+  }
+
+  const curPath = document.location.pathname
+  if (curPath === '/') {
+    presenceData.details = 'Viewing the main page'
+  }
+  else {
+    // /h/nature/post/352201/new-hole-discovered-hnature
+    const parts = curPath.split('/')
+    if (parts.length === 3 && parts[1] === 'h') {
+      // 只有板块，没有帖子
+      const section = `/${parts[1]}/${parts[2]}` // "/h/nature"
+      presenceData.details = `Browsing section: ${section}`
+    }
+    else if (parts.length >= 6 && parts[1] === 'h' && parts[3] === 'post') {
+      // 具体帖子
+      const section = `/${parts[1]}/${parts[2]}` // "/h/nature"
+      const postId = parts[4]
+      const postTitleRaw = parts[5] ?? ''
+      const postTitle = postTitleRaw.replace(/-/g, ' ')
+      presenceData.details = `Watching Post: ${postTitle}`
+      presenceData.state = `From section "${section}", #${postId}`
+      presenceData.buttons = [
+        {
+          label: 'Watch Post',
+          url: document.location.href,
+        },
+      ]
+    }
+    else {
+      // 其他路径处理
+      presenceData.details = `Browsing ${curPath}`
+    }
+  }
+  presence.setActivity(presenceData)
+})
