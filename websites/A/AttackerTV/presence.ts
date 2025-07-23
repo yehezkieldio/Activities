1364c7cc40c11dc0a6672faa222d80565924e53c
PreMiD Automations <contact@premid.app>
2025-07-23 19:19:13 +0000

    chore: update assets

--- websites/A/AttackerTV/presence.ts
+++ websites/A/AttackerTV/presence.ts
@@ -6,7 +6,7 @@ const presence = new Presence({
 const browsingTimestamp = Math.floor(Date.now() / 1000)
 
 enum ActivityAssets {
-  Logo = 'https://imgur.com/v1hnszw.png',
+  Logo = 'https://cdn.rcd.gg/PreMiD/websites/A/AttackerTV/assets/logo.png',
 }
 
 let cacheMovie: {
