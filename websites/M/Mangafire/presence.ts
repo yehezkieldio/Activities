ff0145cec90b1deca5b8158b5a66ee989b55c8ff
PreMiD Automations <contact@premid.app>
2025-07-24 16:23:18 +0000

    chore: update assets

--- websites/M/Mangafire/presence.ts
+++ websites/M/Mangafire/presence.ts
@@ -6,7 +6,7 @@ const presence = new Presence({
 const browsingTimestamp = Math.floor(Date.now() / 1000)
 
 enum ActivityAssets {
-  Logo = 'https://i.imgur.com/GP6ci4C.png',
+  Logo = 'https://cdn.rcd.gg/PreMiD/websites/M/Mangafire/assets/logo.png',
 }
 
 const Fields = {
