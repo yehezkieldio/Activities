48833cf53e5a0d9d97b7abad7a420ee902807485
Jack Dawson <77471397+jqcktalks@users.noreply.github.com>
2025-08-05 23:30:55 +0100

    feat(TruckersFM): change classes to xpath & new branding (#9844)
    new rebanding and new paths

--- websites/T/TruckersFM/presence.ts
+++ websites/T/TruckersFM/presence.ts
@@ -5,18 +5,27 @@ const presence = new Presence({
 })
 const browsingTimestamp = Math.floor(Date.now() / 1000)
 
+function getElementByXPath(xpath: string): Element | null {
+  const result = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null)
+  return result.singleNodeValue as Element | null
+}
+
 presence.on('UpdateData', () => {
   const presenceData: PresenceData = {
-    largeImageKey: document.querySelector<HTMLImageElement>('.album-art')?.src || 'https://cdn.rcd.gg/PreMiD/websites/T/TruckersFM/assets/logo.png',
+    largeImageKey: (getElementByXPath('/html/body/div[1]/div/div[1]/div[3]/div[1]/div/div[1]/div/div[1]/img') as HTMLImageElement)?.src || 'https://i.imgur.com/R5xHjtR.png',
     startTimestamp: browsingTimestamp,
     type: ActivityType.Listening,
   }
 
+  const artistElement = getElementByXPath('/html/body/div[1]/div/div[1]/div[3]/div[1]/div/div[1]/div/div[2]/div[1]/div[2]')
+  const titleElement = getElementByXPath('/html/body/div[1]/div/div[1]/div[3]/div[1]/div/div[1]/div/div[2]/div[1]/div[1]')
+  const presenterElement = getElementByXPath('/html/body/div[1]/div/div[1]/div[3]/div[1]/div/div[2]/div/div[2]/div/div[1]/div[2]/div[1]')
+
   presenceData.details = `${
-    document.querySelector('.player-artist-text')?.textContent
-  } - ${document.querySelector('.player-title-text')?.textContent}`
-  presenceData.state = document.querySelector('.live-name')?.textContent ?? 'AutoDJ'
-  presenceData.smallImageKey = 'https://cdn.rcd.gg/PreMiD/websites/T/TruckersFM/assets/logo.png'
+    artistElement?.textContent
+  } - ${titleElement?.textContent}`
+  presenceData.state = presenterElement?.textContent ?? 'AutoDJ'
+  presenceData.smallImageKey = 'https://i.imgur.com/R5xHjtR.png'
   presenceData.smallImageText = 'TruckersFM'
 
   presence.setActivity(presenceData)
