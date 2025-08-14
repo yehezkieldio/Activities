9e473ebaebcc5cb506e6dc93eea9ddb25d9c8625
PreMiD Automations <contact@premid.app>
2025-08-14 08:30:53 +0000

    chore: update assets

--- websites/W/WitAnime/presence.ts
+++ websites/W/WitAnime/presence.ts
@@ -6,7 +6,7 @@ const presence = new Presence({
 const browsingTimestamp = Math.floor(Date.now() / 1000)
 
 enum ActivityAssets {
-  Logo = 'https://imgur.com/NhnMoGu.png',
+  Logo = 'https://cdn.rcd.gg/PreMiD/websites/W/WitAnime/assets/logo.png',
 }
 
 let cacheEpisode: {
