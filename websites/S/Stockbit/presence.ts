843db47f232f0f0ad46fa88bf935844599f0a988
PreMiD Automations <contact@premid.app>
2025-08-15 21:58:20 +0000

    chore: update assets

--- websites/S/Stockbit/presence.ts
+++ websites/S/Stockbit/presence.ts
@@ -6,7 +6,7 @@ const presence = new Presence({
 const browsingTimestamp = Math.floor(Date.now() / 1000)
 
 enum ActivityAssets {
-  Logo = 'https://i.imgur.com/mrEN3GP.png',
+  Logo = 'https://cdn.rcd.gg/PreMiD/websites/S/Stockbit/assets/logo.png',
 }
 
 presence.on('UpdateData', async () => {
