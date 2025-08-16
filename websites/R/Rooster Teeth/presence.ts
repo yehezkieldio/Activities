4751098d88c0a0f2a55f3afd07070309dabad15f
PreMiD Automations <contact@premid.app>
2025-08-16 13:38:13 +0000

    chore: update assets

--- websites/R/Rooster Teeth/presence.ts
+++ websites/R/Rooster Teeth/presence.ts
@@ -6,7 +6,7 @@ const presence = new Presence({
 const browsingTimestamp = Math.floor(Date.now() / 1000)
 
 enum ActivityAssets {
-  Logo = 'https://i.imgur.com/pO33yHV.png',
+  Logo = 'https://cdn.rcd.gg/PreMiD/websites/R/Rooster%20Teeth/assets/logo.png',
 }
 
 let iFrameData: {
