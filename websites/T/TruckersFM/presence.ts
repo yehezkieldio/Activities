6b2a2741fd2f8a3dd52407c752b3377d92920ff1
Jack Dawson <77471397+jqcktalks@users.noreply.github.com>
2025-08-18 22:43:34 +0100

    feat(TruckersFM): update xpath back to ID's (#9895)
    * new rebanding and new paths
    
    * update to IDs again
    
    * Update metadata.json
    
    * revert logo 🤷
    
    * fix lint
    
    ---------
    
    Signed-off-by: Jack Dawson <77471397+jqcktalks@users.noreply.github.com>

--- websites/T/TruckersFM/presence.ts
+++ websites/T/TruckersFM/presence.ts
@@ -5,26 +5,23 @@ const presence = new Presence({
 })
 const browsingTimestamp = Math.floor(Date.now() / 1000)
 
-function getElementByXPath(xpath: string): Element | null {
-  const result = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null)
-  return result.singleNodeValue as Element | null
-}
-
 presence.on('UpdateData', () => {
   const presenceData: PresenceData = {
-    largeImageKey: (getElementByXPath('/html/body/div[1]/div/div[1]/div[3]/div[1]/div/div[1]/div/div[1]/img') as HTMLImageElement)?.src || 'https://cdn.rcd.gg/PreMiD/websites/T/TruckersFM/assets/logo.png',
+    largeImageKey: document.querySelector<HTMLImageElement>('#song-art')?.src || 'https://cdn.rcd.gg/PreMiD/websites/T/TruckersFM/assets/logo.png',
     startTimestamp: browsingTimestamp,
     type: ActivityType.Listening,
   }
 
-  const artistElement = getElementByXPath('/html/body/div[1]/div/div[1]/div[3]/div[1]/div/div[1]/div/div[2]/div[1]/div[2]')
-  const titleElement = getElementByXPath('/html/body/div[1]/div/div[1]/div[3]/div[1]/div/div[1]/div/div[2]/div[1]/div[1]')
-  const presenterElement = getElementByXPath('/html/body/div[1]/div/div[1]/div[3]/div[1]/div/div[2]/div/div[2]/div/div[1]/div[2]/div[1]')
+  const artistElement = document.getElementById('song-artist')
+  const titleElement = document.getElementById('song-title')
+  const presenterElement = document.getElementById('show-name')
 
-  presenceData.details = `${
-    artistElement?.textContent
-  } - ${titleElement?.textContent}`
-  presenceData.state = presenterElement?.textContent ?? 'AutoDJ'
+  const artist = artistElement?.textContent?.trim() || 'TruckersFM'
+  const title = titleElement?.textContent?.trim() || 'Loading...'
+  const presenter = presenterElement?.textContent?.trim() || 'TruckersFM'
+
+  presenceData.details = `${artist} - ${title}`
+  presenceData.state = presenter
   presenceData.smallImageKey = 'https://cdn.rcd.gg/PreMiD/websites/T/TruckersFM/assets/logo.png'
   presenceData.smallImageText = 'TruckersFM'
 
