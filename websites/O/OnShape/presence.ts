d50d00e01be08faa6d072a3d6dea8189ad575b28
Guhszvv <92408359+Guhszvv@users.noreply.github.com>
2025-08-19 15:22:11 -0300

    feat(OnShape): add presence activity (#9867)
    * Onshape presence
    
    * Linted code
    
    * fix(OnShape): linted code
    
    * fix(OnShape): fix extra semicolon and singlequote
    
    * fix(OnShape): add  missing trailing comma and remove trailing spaces
    
    * fix(OnShape): fix json description

---
+++ websites/O/OnShape/presence.ts
@@ -0,0 +1,40 @@
+const presence = new Presence({
+  clientId: '1403785669171740814',
+})
+
+const browsingTimestamp = Math.floor(Date.now() / 1000)
+
+enum ActivityAssets {
+  Logo = 'https://i.imgur.com/Hi8NUuU.png',
+}
+
+function getCleanOnshapeTitle(): string {
+  const rawTitle = document.title
+  if (rawTitle.includes('Documents')) {
+    return 'Searching...'
+  }
+  return rawTitle.replace(' - Onshape', '').trim()
+}
+
+function getOnshapeStatus(): string {
+  const status = document.title
+  if (status.includes('Documents')) {
+    return ''
+  }
+  else {
+    return 'Editing...'
+  }
+}
+
+presence.on('UpdateData', async () => {
+  const title = getCleanOnshapeTitle()
+  const state = getOnshapeStatus()
+  const presenceData: PresenceData = {
+    largeImageKey: ActivityAssets.Logo,
+    startTimestamp: browsingTimestamp,
+    details: `${title}`,
+    state: `${state}`,
+  }
+
+  presence.setActivity(presenceData)
+})
