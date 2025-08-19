8e080d9f047d902c455c94c41b15445a0d2aaa19
PreMiD Automations <contact@premid.app>
2025-08-19 18:23:08 +0000

    chore: update assets

--- websites/O/OnShape/presence.ts
+++ websites/O/OnShape/presence.ts
@@ -5,7 +5,7 @@ const presence = new Presence({
 const browsingTimestamp = Math.floor(Date.now() / 1000)
 
 enum ActivityAssets {
-  Logo = 'https://i.imgur.com/Hi8NUuU.png',
+  Logo = 'https://cdn.rcd.gg/PreMiD/websites/O/OnShape/assets/logo.png',
 }
 
 function getCleanOnshapeTitle(): string {
