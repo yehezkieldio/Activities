333c1d27c2c506f0f301a8c6504962b5cdcef60b
joerkig <44418429+joerkig@users.noreply.github.com>
2025-08-30 12:42:20 +0200

    chore(Bluesky): update logo (#9932)

--- websites/B/Bluesky/presence.ts
+++ websites/B/Bluesky/presence.ts
@@ -4,7 +4,7 @@ const presence = new Presence({
 
 presence.on('UpdateData', async () => {
   const presenceData: PresenceData = {
-    largeImageKey: 'https://cdn.rcd.gg/PreMiD/websites/B/Bluesky/assets/logo.jpg',
+    largeImageKey: 'https://i.imgur.com/wJW8Z45.png',
   }
   const path = document.location.pathname.split('/')
   const [showUsername, showPost, showFeed] = await Promise.all([
