3f75a9bd665253f004498bae9789a9e9af487efa
PreMiD Automations <contact@premid.app>
2025-09-05 16:45:39 +0000

    chore: update assets

--- websites/S/Sakura AI/types.ts
+++ websites/S/Sakura AI/types.ts
@@ -1,5 +1,5 @@
 export enum Images {
-  Logo = 'https://mido.anlayana.com/premid/images/sakuraai/logo.png',
+  Logo = 'https://cdn.rcd.gg/PreMiD/websites/S/Sakura%20AI/assets/logo.png',
 }
 
 export interface Settings {
