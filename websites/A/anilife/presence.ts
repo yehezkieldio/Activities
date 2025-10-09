889c7bce4597fdafc86d790f112f9320c50dffdc
ICE <65211687+ice3353@users.noreply.github.com>
2025-10-09 16:12:38 +0900

    feat(anilife): update for site update and other improvements (#10044)
    Co-authored-by: ICE <mikotochan@waifu.club>

--- websites/A/anilife/presence.ts
+++ websites/A/anilife/presence.ts
@@ -6,15 +6,15 @@ const presence = new Presence({
 
 presence.on('UpdateData', async () => {
   const presenceData: PresenceData = {
-    largeImageKey: 'https://cdn.rcd.gg/PreMiD/websites/A/anilife/assets/logo.png',
+    largeImageKey: await presence.getSetting('hide') ? null : 'https://cdn.rcd.gg/PreMiD/websites/A/anilife/assets/logo.png',
     type: ActivityType.Watching,
     name: 'Anilife',
   }
   if (document.location.hostname === 'anilife.app') {
-    if (document.location.pathname === '/play') {
+    if (document.location.pathname === '/watch') {
       const videoData = document.querySelector('video')
-      presenceData.name = document.querySelector('h1')?.textContent?.replace(/\s*\d+화$/, '') ?? '(로딩 중)'
-      presenceData.details = Array.from(document.querySelectorAll('b')).reverse().find(b => b.textContent?.includes(document.querySelector('h1')?.textContent?.split(' ').at(-1) ?? '')); // get episode title
+      presenceData.name = document.querySelector('h2.inline-block')?.textContent ?? '(로딩 중)'
+      presenceData.details = document.querySelector('span.line-clamp-3')?.textContent ?? '(로딩 중)';
       [presenceData.startTimestamp, presenceData.endTimestamp] = getTimestampsFromMedia(videoData!)
       presenceData.state = videoData?.paused ? '일시 정지됨' : '재생 중'
       presenceData.smallImageKey = videoData?.paused ? Assets.Pause : Assets.Play
@@ -26,24 +26,25 @@ presence.on('UpdateData', async () => {
         presenceData.state = '탐색 중'
       }
       else if (document.location.pathname.split('/')[1] === 'content') {
-        presenceData.details = `${document.querySelector('h1')?.textContent ?? '(로딩 중)'} 목록`
-        presenceData.state = '탐색 중'
+        presenceData.details = `${document.querySelector('h1.mb-1')?.textContent ?? '(로딩 중)'} 에피소드 목록`
+        presenceData.state = Array.from(document.querySelectorAll('div.mb-2 a span')).map(a => a.textContent).join(', ')
       }
       else if (document.location.pathname === '/results') {
         presenceData.details = `'${document.querySelector('h1')?.textContent?.split('\'')[1]}' 검색 결과`
         presenceData.state = '탐색 중'
         presenceData.smallImageKey = Assets.Search
       }
-      else if (['anime', 'daily', 'complete', 'top', 'random'].includes(document.location.pathname.split('/')[1] ?? '')) {
+      else if (['daily', 'complete', 'top', 'random'].includes(document.location.pathname.split('/')[1] ?? '')) {
         presenceData.details = document.querySelector('h1')?.textContent ?? '(로딩 중)'
         presenceData.state = '탐색 중'
       }
-      else if (document.location.pathname.split('/')[1] === 'season') {
-        presenceData.details = `${document.querySelector('h1')?.textContent?.replace('작품 목록', '') ?? '(로딩 중)'} 애니 목록`
-        presenceData.state = '탐색 중'
+      else if (document.location.pathname.split('/')[1] === 'staff') {
+        presenceData.details = document.querySelector('h1')?.textContent ?? '(로딩 중)'
+        presenceData.state = '성우 정보'
       }
-      else if (document.location.pathname === '/browse') {
-        presenceData.details = '전체 탐색'
+      else if (document.location.pathname.split('/')[1] === 'season') {
+        presenceData.details = `${document.querySelector('h1')?.textContent?.replace('작품 목록', '') ?? '(로딩 중)'} 목록`
+        presenceData.state = '탐색 중'
       }
       else if (document.location.pathname.split('/')[1] === 'archive') {
         presenceData.details = '보관함'
@@ -51,13 +52,10 @@ presence.on('UpdateData', async () => {
       }
       else if (document.location.pathname === '/explore') {
         presenceData.details = '필터 검색 중'
+        presenceData.state = Array.from(document.querySelectorAll('div.inline-flex span.flex-1')).map(a => a.textContent).join(', ')
       }
-      else if (document.location.pathname.startsWith('/notice')) {
+      else if (document.location.pathname.startsWith('/notices')) {
         presenceData.details = '공지사항'
-        presenceData.state = '탐색 중'
-      }
-      else if (document.location.pathname === '/browse/female') {
-        presenceData.details = '여성향 애니 목록'
         presenceData.state = '탐색 중'
       }
     }
@@ -105,8 +103,7 @@ presence.on('UpdateData', async () => {
     }
   }
   if (await presence.getSetting('hide')) { // hide using anilife
-    delete presenceData.largeImageKey
-    if (!['play', 'h', 'search', 'results'].includes(document.location.pathname.split('/')[1] ?? ' '))
+    if (!['watch', 'h', 'search', 'results'].includes(document.location.pathname.split('/')[1] ?? ' '))
       presenceData.largeImageKey = Assets.Play
     if (['anilife', 'Anilife'].includes(presenceData.name ?? ' '))
       presenceData.name = '애니메이션'
